dc7919d94e565febfe7008b3d13f516fcf1de063
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-14 23:18:59 +0900

    style: 초기 카운트다운 타이머 레이아웃 스타일링 추가

---
+++ src/Components/CountdownTimer.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const CountdownTimer = () => {
+  return <TimerContainer></TimerContainer>;
+};
+
+export default CountdownTimer;
+
+const TimerContainer = styled.View`
+  width: 140px;
+  height: 140px;
+  background-color: #fbdf60;
+  border-radius: 20px;
+  padding: 15px;
+`;
