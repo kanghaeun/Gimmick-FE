a95861695020f15d157be5c653a6d6fcce1fee51
kanghaeun <xgkg0330@jnu.ac.kr>
2024-11-14 23:33:23 +0900

    style: 카운트다운 타이머 헤더 부분 스타일링 추가
    - 음식 아이콘과 오른쪽 화살표 아이콘 추가

--- src/Components/CountdownTimer.jsx
+++ src/Components/CountdownTimer.jsx
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Entypo';
 
 const CountdownTimer = () => {
-  return <TimerContainer></TimerContainer>;
+  return (
+    <TimerContainer>
+      <TimerHeaderWrapper>
+        <IconboxWrapper>
+          <IconView>🍔</IconView>
+        </IconboxWrapper>
+        <Icon name="chevron-right" size={40} color="#61734D" />
+      </TimerHeaderWrapper>
+    </TimerContainer>
+  );
 };
 
 export default CountdownTimer;
@@ -13,4 +23,25 @@ const TimerContainer = styled.View`
   background-color: #fbdf60;
   border-radius: 20px;
   padding: 15px;
+`;
+
+const TimerHeaderWrapper = styled.View`
+  font-size: 24px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const IconboxWrapper = styled.View`
+  width: 40px;
+  height: 40px;
+  background-color: #ffffff;
+  border-radius: 13px;
+  opacity: 0.7;
+  justify-content: center;
+  align-items: center;
+`;
+
+const IconView = styled.Text`
+  font-size: 24px;
 `;
